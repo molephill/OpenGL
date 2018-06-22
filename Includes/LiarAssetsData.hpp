18de406d983a5e6f7b43a5ebfe8429635e732fbc
Liar <330446104@qq.com>
2018-06-22 18:19:19 +0800

    Assets Manager

--- Includes/LiarAssetsData.hpp
+++ Includes/LiarAssetsData.hpp
@@ -11,13 +11,15 @@
 #define LiarAssetsData_hpp
 
 #include <string>
+#include <iostream>
+#include <stb_image.h>
 
 namespace Liar
 {
 	class LiarBaseAssetsData
 	{
 	public:
-		LiarBaseAssetsData();
+		LiarBaseAssetsData(std::string& path);
 		~LiarBaseAssetsData();
 
 	protected:
@@ -28,6 +30,47 @@ namespace Liar
 		void IncRefCount() { ++m_refCount; };
 		int Release() { return --m_refCount; };
 		bool CheckSamePath(const std::string& val) { return val == m_path; };
+	};
+
+
+	// ==============   texture_data ================
+	class LiarTextureData:public LiarBaseAssetsData
+	{
+	public:
+		LiarTextureData(std::string&, int);
+		~LiarTextureData();
+
+	private:
+		int m_width;
+		int m_height;
+		int m_nrChannels;
+		int m_rgb_mod;
+		unsigned char* m_data;
+
+	public:
+	public:
+		int GetWidth() const { return m_width; };
+		int GetHeight() const { return m_height; };
+		int GetNrChannels() const { return m_nrChannels; };
+		int GetRgbMod() const { return m_rgb_mod; };
+		unsigned char* GetData() const { return m_data; };
+	};
+
+	// ==============   texture2d  ================
+	class LiarTexture2D :public LiarBaseAssetsData
+	{
+	public:
+		LiarTexture2D(std::string&, int, int, int);
+		~LiarTexture2D();
+
+	private:
+		unsigned int m_id;
+
+		int m_wrapMod;
+		int m_levMod;
+
+	public:
+		void UpLoad();
 	};
 }
 
--- Load/LiarAssetsData.cpp
+++ Load/LiarAssetsData.cpp
@@ -2,12 +2,24 @@
 
 namespace Liar
 {
-	LiarBaseAssetsData::LiarBaseAssetsData()
+	LiarBaseAssetsData::LiarBaseAssetsData(std::string& path)
+		:m_path(path), m_refCount(1)
 	{
 	}
 
 
 	LiarBaseAssetsData::~LiarBaseAssetsData()
 	{
+	}
+
+	// ======================= texture_data =======================
+	LiarTextureData::LiarTextureData(std::string& path, int type) :LiarBaseAssetsData(path), m_rgb_mod(type)
+	{
+		m_data = stbi_load(m_path.c_str(), &m_width, &m_height, &m_nrChannels, 0);
+	}
+
+	LiarTextureData::~LiarTextureData()
+	{
+		stbi_image_free(m_data);
 	}
 }
