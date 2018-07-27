25663ca2d87967608eda7796df6217ffdeb997b6
liar <330446104@qq.com>
2018-07-27 16:47:17 +0800

    Load LiarMesh

--- Render/RenderMgr.cpp
+++ Render/RenderMgr.cpp
@@ -15,14 +15,6 @@
 
 namespace Liar
 {
-	class TestVertex
-	{
-	public:
-		Liar::Vector3D position;
-		Liar::Vector3D normal;
-		Liar::Vector3D color;
-		Liar::Vector2D uv;
-	};
 
 	RenderMgr::RenderMgr():m_red(0.2f),m_green(0.3f),m_blue(0.3f)
 	{
@@ -31,23 +23,106 @@ namespace Liar
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 		m_camera = new Liar::Camera3D();
-		m_camera->SetPosition(0, 0.0f, 10.0f);
+		m_camera->SetPosition(0, 0.0f, 20.0f);
 		//m_camera->SetRotationX(26.0f);
 
 
-		float vertices[] = {
-			// positions          // colors								// texture coords
-			0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-			0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-			-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-			-0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
-		};
 		unsigned int indices[] = {
 			0, 1, 3, // first triangle
 			1, 2, 3  // second triangle
 		};
 
-		std::vector<Liar::LiarVertexBuffer*>* vertices2 = new std::vector<LiarVertexBuffer*>();
+		//std::vector<Liar::LiarVertexBuffer*>* vertices2 = new std::vector<LiarVertexBuffer*>();
+		//Liar::LiarVertexBuffer* v = new Liar::LiarVertexBuffer();
+		//v->position->Set(0.5f, 0.5f, 0.0);
+		//v->color->Set(1.0f, 0.0f, 0.0f);
+		//v->normal->Set(1.0f, 0.0f, 0.0f);
+		//v->uv->Set(1.0f, 1.0f);
+		//vertices2->push_back(v);
+
+		//Liar::LiarVertexBuffer* v1 = new Liar::LiarVertexBuffer();
+		//v1->position->Set(0.5f, -0.5f, 0.0f);
+		//v1->color->Set(0.0f, 1.0f, 0.0f);
+		//v1->normal->Set(1.0f, 0.0f, 0.0f);
+		//v1->uv->Set(1.0f, 0.0f);
+		//vertices2->push_back(v1);
+
+		//Liar::LiarVertexBuffer* v2 = new Liar::LiarVertexBuffer();
+		//v2->position->Set(-0.5f, -0.5f, 0.0f);
+		//v2->color->Set(0.0f, 0.0f, 1.0f);
+		//v2->normal->Set(1.0f, 0.0f, 0.0f);
+		//v2->uv->Set(0.0f, 0.0f);
+		//vertices2->push_back(v2);
+
+		//Liar::LiarVertexBuffer* v3 = new Liar::LiarVertexBuffer();
+		//v3->position->Set(-0.5f, 0.5f, 0.0f);
+		//v3->color->Set(1.0f, 1.0f, 0.0f);
+		//v3->normal->Set(1.0f, 0.0f, 0.0f);
+		//v3->uv->Set(0.0f, 1.0f);
+		//vertices2->push_back(v3);
+
+		//std::vector<unsigned int>* indices1 = new std::vector<unsigned int>();
+		//size_t indicesLen = sizeof(indices) / sizeof(unsigned int);
+		//for (size_t i = 0; i < indicesLen; ++i)
+		//{
+		//	indices1->push_back(indices[i]);
+		//}
+
+		//size_t indiceSize1 = indices1->size() * sizeof(unsigned int);
+
+		//glGenVertexArrays(1, &m_VAO);
+		//glGenBuffers(1, &m_VBO);
+		//glGenBuffers(1, &m_EBO);
+
+		//glBindVertexArray(m_VAO);
+
+		//glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+		////glBufferData(GL_ARRAY_BUFFER, buffSize3, vertices3->data(), GL_STATIC_DRAW);
+
+		//size_t positionSize = sizeof(Liar::Vector3D);
+		//size_t normalSize = sizeof(Liar::Vector3D);
+		//size_t colorSize = sizeof(Liar::Vector3D);
+		//size_t uvSize = sizeof(Liar::Vector2D);
+
+		//size_t oneSize = positionSize + normalSize + colorSize + uvSize;
+		//size_t vertexSize = vertices2->size();
+		//size_t totalSize = vertexSize * oneSize;
+
+		//size_t normalOffSize = positionSize;
+		//size_t colorOffSize = positionSize + normalOffSize;
+		//size_t uvOffSize = colorOffSize + colorSize;
+
+		//glBufferData(GL_ARRAY_BUFFER, totalSize, nullptr, GL_STATIC_DRAW);
+		//for (size_t i = 0; i < vertexSize; ++i)
+		//{
+		//	size_t start = i * oneSize;
+		//	Liar::LiarVertexBuffer* buffData = vertices2->at(i);
+		//	glBufferSubData(GL_ARRAY_BUFFER, start, positionSize, buffData->position);
+		//	glBufferSubData(GL_ARRAY_BUFFER, start + normalOffSize, normalSize, buffData->normal);
+		//	glBufferSubData(GL_ARRAY_BUFFER, start + colorOffSize, colorSize, buffData->color);
+		//	glBufferSubData(GL_ARRAY_BUFFER, start + uvOffSize, uvSize, buffData->uv);
+		//}
+
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+		//glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiceSize1, indices1->data(), GL_STATIC_DRAW);
+
+		//// position attribute
+		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, oneSize, (void*)0);
+		//glEnableVertexAttribArray(0);
+		//// color attribute
+		//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, oneSize, (void*)normalOffSize);
+		//glEnableVertexAttribArray(1);
+		//// color attribute
+		//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, oneSize, (void*)colorOffSize);
+		//glEnableVertexAttribArray(2);
+		//// texture coord attribute
+		//glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, oneSize, (void*)uvOffSize);
+		//glEnableVertexAttribArray(3);
+
+		m_testMesh = Liar::LiarMeshRead::ReadMesh("C:/Users/Administrator/Desktop/nanosuit/nano_body.mesh");
+		/*m_testMesh = new Liar::LiarMesh();
+		std::vector<Liar::LiarVertexBuffer*>* vertices2 = m_testMesh->GetGeo()->GetBuffers();
+
 		Liar::LiarVertexBuffer* v = new Liar::LiarVertexBuffer();
 		v->position->Set(0.5f, 0.5f, 0.0);
 		v->color->Set(1.0f, 0.0f, 0.0f);
@@ -57,7 +132,7 @@ namespace Liar
 
 		Liar::LiarVertexBuffer* v1 = new Liar::LiarVertexBuffer();
 		v1->position->Set(0.5f, -0.5f, 0.0f);
-		v1->color->Set(1.0f, 1.0f, 0.0f);
+		v1->color->Set(0.0f, 1.0f, 0.0f);
 		v1->normal->Set(1.0f, 0.0f, 0.0f);
 		v1->uv->Set(1.0f, 0.0f);
 		vertices2->push_back(v1);
@@ -75,121 +150,18 @@ namespace Liar
 		v3->normal->Set(1.0f, 0.0f, 0.0f);
 		v3->uv->Set(0.0f, 1.0f);
 		vertices2->push_back(v3);
+		
 
+		m_testMesh->GetGeo()->SetBufferSize(4);
 
-		// ==============================================================================
-		std::vector<Liar::TestVertex>* vertices3 = new std::vector<TestVertex>();
-		Liar::TestVertex v3_1;
-		v3_1.position.Set(0.5f, 0.5f, 0.0);
-		v3_1.color.Set(1.0f, 0.0f, 0.0f);
-		v3_1.normal.Set(1.0f, 0.0f, 0.0f);
-		v3_1.uv.Set(1.0f, 1.0f);
-		vertices3->push_back(v3_1);
-
-		Liar::TestVertex v3_2;
-		v3_2.position.Set(0.5f, -0.5f, 0.0f);
-		v3_2.color.Set(1.0f, 1.0f, 0.0f);
-		v3_2.normal.Set(1.0f, 0.0f, 0.0f);
-		v3_2.uv.Set(1.0f, 0.0f);
-		vertices3->push_back(v3_2);
-
-		Liar::TestVertex v3_3;
-		v3_3.position.Set(-0.5f, -0.5f, 0.0f);
-		v3_3.color.Set(0.0f, 0.0f, 1.0f);
-		v3_3.normal.Set(1.0f, 0.0f, 0.0f);
-		v3_3.uv.Set(0.0f, 0.0f);
-		vertices3->push_back(v3_3);
-
-		Liar::TestVertex v3_4;
-		v3_4.position.Set(-0.5f, 0.5f, 0.0f);
-		v3_4.color.Set(1.0f, 1.0f, 0.0f);
-		v3_4.normal.Set(1.0f, 0.0f, 0.0f);
-		v3_4.uv.Set(0.0f, 1.0f);
-		vertices3->push_back(v3_4);
-		// ==============================================================================
-
-
-		std::vector<float>* vertices1 = new std::vector<float>();
-		size_t len = sizeof(vertices) / sizeof(float);
-		for (size_t i = 0; i < len; ++i)
-		{
-			vertices1->push_back(vertices[i]);
-		}
-
-		std::vector<unsigned int>* indices1 = new std::vector<unsigned int>();
+		std::vector<unsigned int>* indices1 = m_testMesh->GetGeo()->GetIndices();
 		size_t indicesLen = sizeof(indices) / sizeof(unsigned int);
 		for (size_t i = 0; i < indicesLen; ++i)
 		{
 			indices1->push_back(indices[i]);
-		}
+		}*/
 
-		size_t buffSize = sizeof(vertices);
-		size_t buffSize1 = vertices1->size() * sizeof(float);
-		
-		//size_t one = sizeof(Liar::Vector3D) * 3 + sizeof(Liar::Vector2D);
-		size_t one = sizeof(Liar::LiarVertexBuffer);
-		size_t buffSize2 = vertices2->size() * one;
-
-		size_t one1 = sizeof(Liar::TestVertex);
-		size_t buffSize3 = vertices3->size() * one1;
-
-		size_t indiceSize = sizeof(indices);
-		size_t indiceSize1 = indices1->size() * sizeof(unsigned int);
-
-		glGenVertexArrays(1, &m_VAO);
-		glGenBuffers(1, &m_VBO);
-		glGenBuffers(1, &m_EBO);
-
-		glBindVertexArray(m_VAO);
-
-		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-		glBufferData(GL_ARRAY_BUFFER, buffSize3, vertices3->data(), GL_STATIC_DRAW);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiceSize1, indices1->data(), GL_STATIC_DRAW);
-
-		//// position attribute
-		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)0);
-		//glEnableVertexAttribArray(0);
-		//// color attribute
-		//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(3 * sizeof(float)));
-		//glEnableVertexAttribArray(1);
-		//// color attribute
-		//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(6 * sizeof(float)));
-		//glEnableVertexAttribArray(2);
-		//// texture coord attribute
-		//glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(9 * sizeof(float)));
-		//glEnableVertexAttribArray(3);
-
-		/*size_t n_offset = offsetof(TestVertex, normal);
-		size_t c_offset = offsetof(TestVertex, color);
-		size_t u_offset = offsetof(TestVertex, uv);*/
-
-		size_t n_offset = offsetof(LiarVertexBuffer, normal);
-		size_t c_offset = offsetof(LiarVertexBuffer, color);
-		size_t u_offset = offsetof(LiarVertexBuffer, uv);
-
-		size_t setSize = one;
-
-		size_t step1 = GL_UNSIGNED_BYTE;
-		size_t step2 = GL_BYTE;
-		size_t step3 = GL_FLOAT;
-
-		// position attribute
-		glVertexAttribPointer(0, 4, GL_UNSIGNED_BYTE, GL_FALSE, setSize, (void*)0);
-		glEnableVertexAttribArray(0);
-		// color attribute
-		glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_FALSE, setSize, (void*)n_offset);
-		glEnableVertexAttribArray(1);
-		// color attribute
-		glVertexAttribPointer(2, 4, GL_UNSIGNED_BYTE, GL_FALSE, setSize, (void*)c_offset);
-		glEnableVertexAttribArray(2);
-		// texture coord attribute
-		glVertexAttribPointer(3, 4, GL_UNSIGNED_BYTE, GL_FALSE, setSize, (void*)u_offset);
-		glEnableVertexAttribArray(3);
-
-		/*m_testMesh = Liar::LiarMeshRead::ReadMesh("C:/Users/Administrator/Desktop/nanosuit/nano_visor.mesh");
-		m_testMesh->Upload();*/
+		m_testMesh->Upload();
 
 		
 
@@ -224,10 +196,10 @@ namespace Liar
 		m_shader->Use();
 		m_shader->SetMat4("projection", *(m_camera->GetMatrix()));
 
-		//m_testMesh->Render(*m_shader);
+		m_testMesh->Render(*m_shader);
 
-		glBindVertexArray(m_VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		/*glBindVertexArray(m_VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
 		//glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
     }
 }
