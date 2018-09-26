2d8607919eb8298b35267666e4c3a743eb22bba9
liar <330446104@qq.com>
2018-09-26 21:38:07 +0800

    调整结构 & 增加灯光

--- Assets/Shaders/test1.fs
+++ Assets/Shaders/test1.fs
@@ -7,9 +7,6 @@ in vec2 TextCoord;
 uniform sampler2D texture0;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
-uniform sampler2D texture3;
-uniform sampler2D texture4;
-uniform sampler2D texture5;
 
 void main()
 {
